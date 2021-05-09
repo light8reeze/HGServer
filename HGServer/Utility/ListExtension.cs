5ebe1122331ded997a86a6043e31c4566c92b586
light8reeze <light8reeze@gmail.com>
2021-05-09 21:56:31 +0900

    feat: ListExtension.cs 추가

---
+++ HGServer/Utility/ListExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace HGServer.Utility
+{
+    static class ListExtension
+    {
+        class ListAccessor<T>
+        {
+            public static Func<List<T>, T[]> Getter;
+
+            static ListAccessor()
+            {
+                var dm = new DynamicMethod("get", MethodAttributes.Static | MethodAttributes.Public, CallingConventions.Standard, typeof(T[]), new Type[] { typeof(List<T>) }, typeof(ListAccessor<T>), true);
+                var il = dm.GetILGenerator();
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Ldfld, typeof(List<T>).GetField("_items", BindingFlags.NonPublic | BindingFlags.Instance)); // Replace argument by field
+                il.Emit(OpCodes.Ret);
+                Getter = (Func<List<T>, T[]>)dm.CreateDelegate(typeof(Func<List<T>, T[]>));
+            }
+        }
+
+        public static T[] GetListArray<T>(this List<T> list)
+        {
+            return ListAccessor<T>.Getter(list);
+        }
+    }
+}
